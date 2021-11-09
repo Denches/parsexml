4e6b9ba43c3d539332a57e5a325b1c9dd8600fb5
Denches <Denkyver@gmail.com>
2021-11-09 13:00:19 +0300

    fix

--- src/main/java/com/exhomework/Parser/SaxParserHandler.java
+++ src/main/java/com/exhomework/Parser/SaxParserHandler.java
@@ -48,7 +48,6 @@ public class SaxParserHandler extends DefaultHandler {
                 }
             }
         }
-        isFile = false;
         currentTamName = null;
         buffer.setLength(0);
     }
