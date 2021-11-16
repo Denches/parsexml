838f53cc9a55aa1410b2bd4ea15130bc96679a53
Denches <Denkyver@gmail.com>
2021-11-17 01:18:45 +0300

    fix

--- src/test/java/com/exhomework/comparator/Initialization.java
+++ src/test/java/com/exhomework/comparator/Initialization.java
@@ -1,14 +1,12 @@
 package com.exhomework.comparator;
 
 import com.exhomework.domain.ArgumentList;
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 
 public class Initialization {
 
     protected Comparator comparator;
     protected ArgumentList argument = new ArgumentList() {};
-
 
     @AfterEach
     void tearDown(){
